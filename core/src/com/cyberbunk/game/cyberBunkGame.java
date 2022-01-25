157e6e42f30174c55149970c45ba7072620f4843
gapspaceengineers <gapspace@engineer.com>
2022-01-25 21:47:02 +0000

    First commit.

---
+++ core/src/com/cyberbunk/game/cyberBunkGame.java
@@ -0,0 +1,23 @@
+package com.cyberbunk.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class cyberBunkGame extends Game {
+
+	@Override
+	public void create () {
+   	}
+
+	@Override
+	public void render () {
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
