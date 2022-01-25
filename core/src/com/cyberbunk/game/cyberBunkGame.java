92221174074942e31d8fff783b57a6fe0fb78f5c
gapspaceengineers <gapspace@engineer.com>
2022-01-25 21:48:25 +0000

    First commit.

--- core/src/com/cyberbunk/game/cyberBunkGame.java
+++ core/src/com/cyberbunk/game/cyberBunkGame.java
@@ -1,11 +1,6 @@
 package com.cyberbunk.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class cyberBunkGame extends Game {
 
---
+++ desktop/src/com/cyberbunk/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cyberbunk.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cyberbunk.game.cyberBunkGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new cyberBunkGame(), config);
+	}
+}
